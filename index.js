ab52b18749b58926b9e2ffd49578d5019d92567b
yue <yinxx325@d.umn.edu>
2019-06-24 17:35:58 -0500

    solve bug about can not find number type, change code to ES6 by using ESlint

--- index.js
+++ index.js
@@ -1,80 +1,81 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongodb = require('./mongoDBFunction.js');
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./mongoDBFunction.js');
 
-var app = express();
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-//app.use(bodyParser.urlencoded({ extended: false }));
-
+const app = express();
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 //  not finish
-app.get('/allQuestions',function(req,res){
-    var responseObject = [{
-        name: "haha"
-    },{
-        name: "yue"
-    }];
-    res.send(responseObject);
-})
-
+app.get('/allQuestions', function(req, res) {
+  const responseObject = [
+    {
+      name: 'haha'
+    },
+    {
+      name: 'yue'
+    }
+  ];
+  res.send(responseObject);
+});
 
 //  example:
 //  http://localhost:3000//findQuestion/abc
-app.get('/findQuestion/:id',function(req,res){
-    console.log("try to find: " + req.params.id);
-    mongodb.findQuestion(req.params.id, function(result){
-        var question = result[0];
-        if(question === undefined){
-            console.log("Not find.")
-            var jsonResponse = {
-                "notification":"can not find"
-            };
-            res.json(jsonResponse);
-
-        }else{
-        console.log("Found: " + req.query.id);
-        res.body = JSON.stringify(question.oneQuestion);
-        res.send(res.body);
+app.get('/findQuestion/:id', function(req, res) {
+  console.log(`try to find: ${req.params.id}`);
+  mongodb.findQuestion(req.params.id, function(result) {
+    const question = result[0];
+    if (question === undefined) {
+      console.log('Not find.');
+      const jsonResponse = {
+        notification: 'can not find'
+      };
+      res.json(jsonResponse);
+    } else {
+      console.log(`Found: ${req.params.id}`);
+      res.body = JSON.stringify(question.oneQuestion);
+      res.send(res.body);
     }
-    });
-})
+  });
+});
 
+app.post('/postOneQuestionByJSON', jsonParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  const oneQuestion = {
+    id: req.body.id.toString(),
+    name: req.body.name,
+    category: req.body.category,
+    question: req.body.question
+  };
+  mongodb.insertQuestion(oneQuestion);
+  console.dir(req.body);
+  const jsonResponse = {
+    id: '123',
+    status: 'updated'
+  };
+  res.json(jsonResponse);
+});
 
-
-app.post('/postOneQuestionByJSON',jsonParser,function(req, res){
-    if (!req.body)
-        return res.sendStatus(400);
-    var oneQuestion = {
-        id: req.body.id,
-        name: req.body.name,
-        category: req.body.category,
-        question: req.body.question
-    };
-    mongodb.insertQuestion(oneQuestion);
-    console.dir(req.body);
-    var jsonResponse = {
-        id: '123', status: 'updated'
-    };
-    res.json(jsonResponse);
-})
-
-app.post('/postOneQuestionByForm',urlencodedParser,function(req, res){
-    if (!req.body)
-        return res.sendStatus(400);
-    var oneQuestion = {
-        id: req.body.id,
-        name: req.body.name,
-        category: req.body.category,
-        question: req.body.question
-    };
-    mongodb.insertQuestion(oneQuestion);
-    console.dir(req.body);
-    var jsonResponse = {
-        id: '123', status: 'updated'
-    };
-    res.json(jsonResponse);
-})
+app.post('/postOneQuestionByForm', urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  const oneQuestion = {
+    id: req.body.id,
+    name: req.body.name,
+    category: req.body.category,
+    question: req.body.question
+  };
+  mongodb.insertQuestion(oneQuestion);
+  console.dir(req.body);
+  const jsonResponse = {
+    id: '123',
+    status: 'updated'
+  };
+  res.json(jsonResponse);
+});
 
 app.listen(3000);
 console.log('listening to port 3000');
