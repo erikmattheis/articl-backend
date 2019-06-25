dd25c94bb674b59ede6acb5b24135f4c4eb474ec
erikmattheis <erik@mattheis.org>
2019-06-25 14:34:51 -0500

    Reduce code to one example, send meaningful response to client, fix ESLint problems

--- index.js
+++ index.js
@@ -1,80 +1,12 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-console */
-/* eslint-disable func-names */
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./mongoDBFunction.js');
 
 const app = express();
 const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-// app.use(bodyParser.urlencoded({ extended: false }));
 
-//  not finish
-app.get('/allQuestions', function(req, res) {
-  const responseObject = [
-    {
-      name: 'haha'
-    },
-    {
-      name: 'yue'
-    }
-  ];
-  res.send(responseObject);
-});
-
-//  example:
-//  http://localhost:3000//findQuestion/abc
-app.get('/findQuestion/:id', function(req, res) {
-  console.log(`try to find: ${req.params.id}`);
-  mongodb.findQuestion(+req.params.id, function(result) {
-    const question = result[0];
-    if (question === undefined) {
-      console.log('Not find.');
-      const jsonResponse = {
-        notification: 'can not find'
-      };
-      res.json(jsonResponse);
-    } else {
-      console.log(`Found: ${req.params.id}`);
-      res.body = JSON.stringify(question.oneQuestion);
-      res.send(res.body);
-    }
-  });
-});
-
-app.post('/postOneQuestionByJSON', jsonParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400);
-  const oneQuestion = {
-    id: req.body.id,
-    name: req.body.name,
-    category: req.body.category,
-    question: req.body.question
-  };
-  mongodb.insertQuestion(oneQuestion);
-  console.dir(req.body);
-  const jsonResponse = {
-    id: '123',
-    status: 'updated'
-  };
-  res.json(jsonResponse);
-});
-
-app.post('/postOneQuestionByForm', urlencodedParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400);
-  const oneQuestion = {
-    id: req.body.id,
-    name: req.body.name,
-    category: req.body.category,
-    question: req.body.question
-  };
-  mongodb.insertQuestion(oneQuestion);
-  console.dir(req.body);
-  const jsonResponse = {
-    id: '123',
-    status: 'updated'
-  };
-  res.json(jsonResponse);
+app.post('/questions', jsonParser, function postQuestion(req, res) {
+  return mongodb.insertQuestion(req, res);
 });
 
 app.listen(3000);
--- mongoDBFunction.js
+++ mongoDBFunction.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable func-names */
 const mongojs = require('mongojs');
 
 const url = 'mongodb://127.0.0.1:27017/articleDatabase';
@@ -11,33 +8,28 @@ const mongoDBRef = mongojs(url, collections);
 
 console.log('MongoDB is active.');
 
-function insertQuestion(oneQuestion) {
-  mongoDBRef
-    .collection('questions')
-    .save({ id: oneQuestion.id, oneQuestion }, function(err, result) {
-      if (err || !result)
-        console.log('This question failed to save in database.');
-      else
-        console.log(
-          'This question inserted into questions collection in MongoDB.'
-        );
-    });
+function insertQuestion(req, res) {
+  try {
+    mongoDBRef
+      .collection('questions')
+      .save(
+        { name: req.body.name, category: req.body.category },
+        function insertQuestionResult(err, result) {
+          if (err || !result) {
+            res
+              .status(422)
+              .json({ errors: ['The question failed to save in database.'] });
+          } else {
+            res.status(201).json({
+              message: 'Successfully saved question.',
+              question: result
+            });
+          }
+        }
+      );
+  } catch (e) {
+    res.status(422).json({ errors: e.mapped() });
+  }
 }
 
-function findQuestion(questionId, callback) {
-  mongoDBRef
-    .collection('questions')
-    .find({ id: questionId })
-    .toArray(function(err, docs) {
-      if (!err) {
-        console.log('Found the following records');
-        console.log(docs);
-        callback(docs);
-      } else console.log('Question not found.');
-    });
-}
-
-function getCollection(collectionName, callback) {}
 module.exports.insertQuestion = insertQuestion;
-module.exports.findQuestion = findQuestion;
-module.exports.getCollection = getCollection;
