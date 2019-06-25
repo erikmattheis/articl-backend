3849e8039e3f41d8ecd978fc3f6485e131a62668
erikmattheis <erik@mattheis.org>
2019-06-25 17:33:56 -0500

    Add helmet to help secure app by setting various HTTP headers

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongodb = require('./mongoDBFunction.js');
 
 const app = express();
+app.use(helmet());
 const jsonParser = bodyParser.json();
 
 app.post('/questions', jsonParser, function postQuestion(req, res) {
-  return mongodb.insertQuestion(req, res);
+  mongodb.insertQuestion(req, res);
 });
 
 app.listen(3000);
