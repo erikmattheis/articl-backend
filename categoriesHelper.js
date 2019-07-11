122d4495eeb07e529a9a55f77869c04e44b337d4
yue <yinxx325@d.umn.edu>
2019-07-11 18:57:40 -0500

    helper

--- categoriesHelper.js
+++ categoriesHelper.js
@@ -1,6 +1,24 @@
-// const fs = require('fs');
+const cacache = require('cacache/en');
+const fs = require('fs');
+const mongodb = require('./mongoDBFunction');
+
+const key = 'my-categories';
+const cachePath = './cache/';
+const { getAllCategories } = require('./mongoDBFunction');
 
 
+async function categories() {
+  const result = await getAllCategories();
+  await cacache.put(cachePath, key, result).then(() => {
+    console.log(`Saved content to ${cachePath}.`);
+  });
+//   cacache.get.stream(
+//     cachePath, 'my-categories',
+//   ).on('data', (data) => {
+//     console.log('get content, [1] is', data.toString());
+//   });
+}
+categories();
 // let data = fs.readFileSync('./category.json', 'utf8');
 // data = JSON.parse(data);
 // const { categories } = data;
@@ -14,4 +32,4 @@
 // console.log('do a helper');
 // console.log(`categories is : ${categories[1]}`);
 
-// module.exports.categories = categories;
+module.exports.categories = categories;
