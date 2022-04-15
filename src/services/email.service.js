c67340d46a6ea101f627da11370f21fae02d9208
erikmattheis <gozz@gozz.com>
2022-04-15 07:01:27 -0500

    Change emailed urls

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,14 +1,18 @@
-const nodemailer = require('nodemailer');
-const config = require('../config/config');
-const logger = require('../config/logger');
+const nodemailer = require("nodemailer");
+const config = require("../config/config");
+const logger = require("../config/logger");
 
 const transport = nodemailer.createTransport(config.email.smtp);
 /* istanbul ignore next */
-if (config.env !== 'test') {
+if (config.env !== "test") {
   transport
     .verify()
-    .then(() => logger.info('Connected to email server'))
-    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+    .then(() => logger.info("Connected to email server"))
+    .catch(() =>
+      logger.warn(
+        "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
+      )
+    );
 }
 
 /**
@@ -30,9 +34,9 @@ const sendEmail = async (to, subject, text) => {
  * @returns {Promise}
  */
 const sendResetPasswordEmail = async (to, token) => {
-  const subject = 'Reset password';
+  const subject = "Reset password";
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `https://api.articl.net/reset-password?token=${token}`;
+  const resetPasswordUrl = `https://articl-vue-2022.herokuapp.com/reset-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
@@ -46,9 +50,9 @@ If you did not request any password resets, then ignore this email.`;
  * @returns {Promise}
  */
 const sendVerificationEmail = async (to, token) => {
-  const subject = 'Email Verification';
+  const subject = "Email Verification";
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `https://api.articl.net/verify-email?token=${token}`;
+  const verificationEmailUrl = `https://articl-vue-2022.herokuapp.com/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
