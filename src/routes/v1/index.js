e7d8f6b9b1eb58d0ac3c394cb7cb0d8efc9f00fa
erikmattheis <gozz@gozz.com>
2022-11-19 01:03:05 -0600

    routing, change /d to /resource

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -24,7 +24,7 @@ const defaultRoutes = [
     route: userRoute,
   },
   {
-    path: "/d",
+    path: "/resource",
     route: articlPages,
   },
   {
@@ -35,7 +35,7 @@ const defaultRoutes = [
     path: "/notes",
     route: notesRoute,
   },
-  
+
 ];
 
 defaultRoutes.forEach((route) => {
