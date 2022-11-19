09e4e85c8e9a32c5a52ca45a19bbda6deb8873a8
erikmattheis <gozz@gozz.com>
2022-11-19 15:45:16 -0600

    Make pssword complexity match front end

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -1,3 +1,5 @@
+const varValueConvert = require('cross-env/src/variable');
+
 const objectId = (value, helpers) => {
   if (!value.match(/^[0-9a-fA-F]{24}$/) && value !== "me") {
     return helpers.message('"{{#label}}" must be a valid mongo id');
@@ -5,17 +7,51 @@ const objectId = (value, helpers) => {
   return value;
 };
 
-const password = (value, helpers) => {
-  if (value.length < 8) {
-    return helpers.message("password must be at least 8 characters");
-  }
-  if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-    return helpers.message(
-      "password must contain at least 1 letter and 1 number"
-    );
+function password(value, helpers) {
+
+  if (scoreChars(value) < 3) {
+
+    helpers.message("Please choose a more complex password. Use a combination upper and lowercase letters, numbers and special characters.");
+
+
+  } else if (value && value.length < 8) {
+
+    helpers.message("Please choose a password. with at least eight characters");
+
+    passed = false;
+
   }
   return value;
 };
+
+function charCounts(val) {
+
+  return {
+    numUpper: val.length - val.replace(/[A-Z]/g, "").length,
+    numLower: val.length - val.replace(/[a-z]/g, "").length,
+    numDigit: val.length - val.replace(/d/g, "").length,
+    numSpecial: val.length - val.replace(/\W|_/g, "").length,
+  };
+
+};
+
+function scoreChars(val) {
+
+  if (!val) {
+
+    return 0;
+
+  }
+
+  const chars = charCounts(val);
+  const a = chars.numUpper > 0 ? 1 : 0;
+  const b = chars.numLower > 0 ? 1 : 0;
+  const c = chars.numDigit > 0 ? 1 : 0;
+  const d = chars.numSpecial > 0 ? 1 : 0;
+
+  return a + b + c + d;
+
+}
 
 module.exports = {
   objectId,
