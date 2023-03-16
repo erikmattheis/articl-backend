37ac4c71e69f03a19d8770a74076a1daae5b7c78
erikmattheis <gozz@gozz.com>
2023-03-16 00:58:53 -0500

    Handle error from ChatGPT better

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -6,15 +6,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const getAISummary = async (category, parentCategory) => {
-  
-  const prompt = `${category} is a medical topic related to ${parentCategory}`;
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-  });
-
-  return completion;
-
+  try {
+    const prompt = `${category} is a medical topic related to ${parentCategory}`;
+    const message = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+    });
+    return {
+      status: 200,
+      message
+    }
+  }
+  catch (err) {
+    return {
+      status: err.response?.status,
+      message: 'The AI service returned an error code of ' + err.response?.status + ' and the message ' + err.message
+    }
+  }
 }
 
 module.exports = {
